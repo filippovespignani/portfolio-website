7da525b24a348d0fcf3ba26d1bea9c62477f8079
filippovespignani <fvespignani1@gmail.com>
2024-02-20 22:15:40 +0100

    primo big commit
    implementazione massiccia tutorial
    - modifiche al layout, soprattutto mobile
    - adattamento a font e tema scuro
    - gestione CSS globale

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function toggleMenu() {
+    
+    //seleziono gli oggetti che sono quelli dell'hamberger menu
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+
+    //abilito o disabilito la classe CSS "open" applicata agli oggetti
+    //ed essendo una funzione toggle, se già presente si occupa di rimuoverla
+    //dandoci cosi entrambe le funzioni di entrata ed uscita del pulsante
+    //con un unica funzione
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
